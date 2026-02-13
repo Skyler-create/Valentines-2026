657545980c58133773aea11e5d23384568f75d6c
Skyler <Skyler.scooty@gmail.com>
2026-02-13 02:35:37 -0600

    change time

--- script.js
+++ script.js
@@ -304,7 +304,7 @@ function prevMem() {
 
 function restartAuto() {
   if (memTimer) clearInterval(memTimer);
-  memTimer = setInterval(nextMem, 5500);
+  memTimer = setInterval(nextMem, 4500);
 }
 
 memoryBtn.addEventListener("click", () => {
